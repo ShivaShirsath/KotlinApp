4d24d5650a7b77126cf0f7a7a7975de540b2e9e5
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-01-18 16:04:50 +0530

    Update MainActivity.kt

--- app/src/main/java/ss/KotlinApp/MainActivity.kt
+++ app/src/main/java/ss/KotlinApp/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main.text = "Shiva is Here 🤘\nYou Must Remember, Who am I. 😁"
     }
 }
