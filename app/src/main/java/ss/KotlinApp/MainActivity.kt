cc8c625fdf14f341e0ffc03439c2037158897f6e
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-01-18 16:10:26 +0530

    Update MainActivity.kt

--- app/src/main/java/ss/KotlinApp/MainActivity.kt
+++ app/src/main/java/ss/KotlinApp/MainActivity.kt
@@ -2,6 +2,7 @@ package ss.KotlinApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
